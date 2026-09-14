cdd636125879c694455783aa69059e036fe537b8
pupsmane <korjagin58@gmail.com>
2026-09-14 22:42:49 +0300

    del functions

--- Untitled-1.cs
+++ Untitled-1.cs
@@ -1,16 +1,12 @@
-using System;
 
-class Program
-{
-    static void Main()
-    {
 Console.WriteLine("Введите сторону A"); // По данным сторонам прямоугольника вычислить его периметр, площадь и длину диагонали.
 double a = Convert.ToDouble(Console.ReadLine());
+
 Console.WriteLine("Введите сторону B"); 
 double b = Convert.ToDouble(Console.ReadLine());
+
 double P = 2*(a+b);
 double S = a*b;
 double d = Math.Sqrt(Math.Pow(a,2)+Math.Pow(b,2));
+
 Console.WriteLine($"Периметр {P}, Площадь {S}, Диагональ {d}.");
-    }
-}
